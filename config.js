1987bc0228465a848a7cfe12a842f13e666045c8
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-08 05:00:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SNlAyT7J#b7padX3C5A3mdcSZFlwdsW7trcRDFTV9qLl52c1dEJA",
+SESSION_ID: process.env.SESSION_ID || "zcVUiRZK#CDaLhucy0Q1cCS4_Jpf5JO7uraoFci9cKd3s73d08Y8",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "private",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/g4qzs7.jpg",
