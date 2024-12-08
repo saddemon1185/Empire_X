43bc1ace41ca3de18af90c0198e212222d3b969f
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-08 03:11:35 +0100

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -1,31 +1,32 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
 cmd({
     pattern: "about",
     react: "👑",
-    desc: "get owner dec",
+    desc: "Get owner details",
     category: "main",
     filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let madeMenu = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
+}, async (conn, mek, m, { from, quoted }) => {
+    try {
+        let madeMenu = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
      │     「 𝗠𝗬 𝗜𝗡𝗧𝗥𝗢 」
      │ Name      : ᴏɴʟʏ_ᴏɴᴇ_🥇ᴇᴍᴘɪʀᴇ
      │ Place      : ʟagos/Nigeria
-     │ Gender    :  ᴍᴀʟᴇ
-     │ Age       : 20_
-     │ Phone     : wa.me/+2348078582627
-     │ Youtube   : Youtube.com/only_one_empire
-     │ Status     : Website Developer & Graphic Designer 🔥💫
-     ╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
-`
+     │ Gender     : ᴍᴀʟᴇ
+     │ Age        : 20
+     │ Phone      : wa.me/+2348078582627
+     │ Youtube    : Youtube.com/only_one_empire
+     │ Status     : Website Developer/Graphics Designer
+     ╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄`;
 
-await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
-
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-  
+        await conn.sendMessage(
+            from,
+            { image: { url: config.ALIVE_IMG }, caption: madeMenu },
+            { quoted: mek }
+        );
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { text: `${e}` }, { quoted: mek });
+    }
+});
