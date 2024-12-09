74f70e8c3caae8f8aa905823221d68f4f8ecc742
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-09 16:27:43 +0100

    Create demon1.js

---
+++ plugins/demon1.js
@@ -0,0 +1,28 @@
+const { cmd, commands } = require('../command');
+const { xeontext1 } = require('./toto/xeontext1');  // Destructure to get the xeontext1 variable
+
+cmd({
+    pattern: "demon1",
+    desc: "Bugs demon",
+    category: "bugs",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        // Send the first message from xeontext1.js
+        await conn.sendMessage(from, {
+            text: xeontext1  // Use the imported xeontext1
+        }, { quoted: mek });
+
+        // Set a delay of 2 minutes (120000 milliseconds) to send the second message
+        setTimeout(async () => {
+            await conn.sendMessage(from, {
+                text: "Pause 2 minutes so that bot isn't banned"  // Send the second message
+            }, { quoted: mek });
+        }, 120000);  // 2 minutes delay (120000 ms)
+        
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
