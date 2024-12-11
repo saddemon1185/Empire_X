4eab0ff2bb26ea92f0671bdd21df6eaa3c171fc9
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-11 16:12:50 +0100

    Update plugins

---
+++ plugins/uptime.js
@@ -0,0 +1,39 @@
+const config = require('../config');
+const { cmd } = require('../command');
+const os = require("os");
+
+cmd({
+    pattern: "uptime",
+    desc: "Check bot's uptime.",
+    category: "main",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, pushname, reply }) => {
+    try {
+        // Function to format uptime
+        function formatUptime(seconds) {
+            const days = Math.floor(seconds / (24 * 60 * 60));
+            seconds %= 24 * 60 * 60;
+            const hours = Math.floor(seconds / (60 * 60));
+            seconds %= 60 * 60;
+            const minutes = Math.floor(seconds / 60);
+            seconds = Math.floor(seconds % 60);
+            return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        }
+
+        // Get the formatted uptime
+        const uptime = formatUptime(process.uptime()); // Direct uptime calculation
+
+        // Construct the uptime message
+        const uptimeMessage = `*Empire_X UPTIME*\n\n` +
+            `*Bot has been running for:*\n` +
+            `${uptime}`;
+
+        // Send the message
+        await conn.sendMessage(from, { text: uptimeMessage }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply(`An error occurred: ${e.message || e}`);
+    }
+});
