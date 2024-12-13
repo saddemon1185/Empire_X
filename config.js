f058c537a376ca77f7019da09c5fae5ceca6915c
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-13 07:55:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
+const fs = require('fs');
+require('dotenv').config(); // This loads the .env file automatically
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
 module.exports = {
     OWNER_NUMBER: process.env.OWNER_NUMBER || "2348078582627",
     SESSION_ID: process.env.SESSION_ID || "iVtxTYSD#SGK0EzkUPfNQvOEDAZvLTw5p2N0WpIJchOsQuLXsGuc",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === 'true',
+    AUTO_READ_STATUS: convertToBool(process.env.AUTO_READ_STATUS, 'true'),
     MODE: process.env.MODE || "private",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/efeurhobo/Empire_X/main/lib/assets/empire.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR EMPIRE_V1 IS ONLINE",
     PREFIX: process.env.PREFIX || ".",
-    OWNER_REACT: process.env.OWNER_REACT === 'true',
-    AUTO_REACT: process.env.AUTO_REACT === 'true',
+    OWNER_REACT: convertToBool(process.env.OWNER_REACT, 'true'),
+    AUTO_REACT: convertToBool(process.env.AUTO_REACT, 'true'),
     OWNER_NAME: process.env.OWNER_NAME || "𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
     BOT_NAME: process.env.BOT_NAME || "Empire_X",
 };
