c6a3ebb468f591ee197ad7f448f74575a4b41354
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-13 13:43:39 +0100

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -10,24 +10,29 @@ cmd({
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, reply }) => {
     try {
+        // Check if the message is a quoted message and if it's a view-once message
         if (!quoted || !/viewOnce/.test(quoted.mtype)) {
             return reply("✳️❇️ It's not a View Once message.");
         }
 
-        const mtype = Object.keys(quoted.message)[0];
-        const mediaMessage = quoted.message[mtype];
+        const mtype = Object.keys(quoted.message)[0];  // Get the message type (imageMessage, videoMessage, etc.)
+        const mediaMessage = quoted.message[mtype];    // Extract the media message content
 
-        // Check for view-once content
-        if (!mediaMessage) return reply("Unable to access the content of the view-once message.");
+        // If there's no content in the view-once message
+        if (!mediaMessage) {
+            return reply("Unable to access the content of the view-once message.");
+        }
 
-        // Download the content
+        // Download the media content
         const stream = await downloadContentFromMessage(mediaMessage, mtype.replace("Message", "").toLowerCase());
         let buffer = Buffer.from([]);
         
+        // Collect chunks of the media
         for await (const chunk of stream) {
             buffer = Buffer.concat([buffer, chunk]);
         }
 
+        // Prepare the caption if available
         const caption = mediaMessage.caption || "Here is the content of the view-once message.";
         
         // Send the media back to the user
