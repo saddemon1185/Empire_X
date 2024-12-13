89438fda6275aafa31744368474760a708ad3dbc
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-13 13:52:56 +0100

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -1,5 +1,5 @@
 const { cmd } = require('../command');
-const { downloadMediaMessage } = require('../lib/msg'); // Import the function from lib/msg.js
+const { downloadMediaMessage } = require('../lib/msg'); // Import from lib/msg.js
 
 // Read View Once Message Command
 cmd({
@@ -9,19 +9,21 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, reply }) => {
     try {
+        // Ensure the message is a view-once message
         if (!quoted || !/viewOnce/.test(quoted.mtype)) {
-            return reply("✳️❇️ It's not a View Once message.");
+            return reply("✳️❇️ This is not a View Once message.");
         }
 
         // Download the content from the view-once message
         const { buffer, caption } = await downloadMediaMessage(quoted);
-        
+
         // Send the media back to the user
         const mtype = Object.keys(quoted.message)[0].replace("Message", "").toLowerCase();
         await conn.sendMessage(from, {
             [mtype]: buffer,
             caption
         }, { quoted: mek });
+        
     } catch (error) {
         console.error(error);
         reply(`An error occurred: ${error.message}`);
