e13a8589e840fe7455f58d6a70f8df7fd2ab0d64
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-14 10:56:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 module.exports = {
     OWNER_NUMBER: process.env.OWNER_NUMBER || "2348078582627",
-    SESSION_ID: process.env.SESSION_ID || "PRdBmSCB#_A9K3Nd4FT2Zk2P42z0JK_EC6qwjHKHh5qH2c-TvOoI",
+    SESSION_ID: process.env.SESSION_ID || "fIEDHbyS#bOM1Jr3x-TGE4AHjpozvUqZhJAu__we9izki9NP5aU0",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
     MODE: process.env.MODE || "private",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/efeurhobo/Empire_X/main/lib/assets/empire.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR EMPIRE_V1 IS ONLINE",
     PREFIX: process.env.PREFIX || ".",
     OWNER_REACT: process.env.OWNER_REACT || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
     OWNER_NAME: process.env.OWNER_NAME || "𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
     BOT_NAME: process.env.BOT_NAME || "Empire_X",
 };
