0a8794d963abec8befc546c6473966fc97d4222a
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-14 14:13:41 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -164,3 +164,34 @@ cmd({
         }, { quoted: mek });
     }
 });
+
+// about command's
+cmd({
+    pattern: "about",
+    react: "👑",
+    desc: "Get owner details",
+    category: "owner",
+    filename: __filename
+}, async (conn, mek, m, { from, quoted }) => {
+    try {
+        let madeMenu = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
+     │     「 𝗠𝗬 𝗜𝗡𝗧𝗥𝗢 」
+     │ Name      : ᴏɴʟʏ_ᴏɴᴇ_🥇ᴇᴍᴘɪʀᴇ
+     │ Place      : ʟagos/Nigeria
+     │ Gender     : ᴍᴀʟᴇ
+     │ Age        : 20
+     │ Phone      : wa.me/+2348078582627
+     │ Youtube    : Youtube.com/only_one_empire
+     │ Status     : Website Developer/Graphics Designer
+     ╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄`;
+
+        await conn.sendMessage(
+            from,
+            { image: { url: config.ALIVE_IMG }, caption: madeMenu },
+            { quoted: mek }
+        );
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { text: `${e}` }, { quoted: mek });
+    }
+});
