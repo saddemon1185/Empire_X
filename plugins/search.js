3c7556a80fea5db1cdeea5bf573b7dba1064f078
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-15 17:39:07 +0100

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -91,7 +91,7 @@ Cloning Repository: ${repoUrl}
     }
 });
 
-// Screenshot Command
+//ss commands 
 cmd({
     pattern: "ss",
     desc: "Screenshot a website",
@@ -101,14 +101,14 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, reply }) => {
     try {
         if (!q) return reply("Please send the website URL to screenshot.");
-        
+
         const url = q.trim();
         if (!/^https?:\/\//.test(url)) {
             return reply("Please enter a valid URL starting with http:// or https://");
         }
 
-        // Screenshot API endpoint with API key, ensuring light mode
-        const screenshotApi = `https://shot.screenshotapi.net/screenshot?token=JX5MFN7-S9AM3HB-HBFEQ00-6BPYHR6&full_page=true&url=${encodeURIComponent(url)}&fresh=true&output=image&file_type=png&dark_mode=false&wait_for_event=load&delay=2000`;
+        // Screenshot API endpoint with API key
+        const screenshotApi = `https://api.giftedtech.my.id/api/tools/sstab?apikey=gifted&url=${encodeURIComponent(url)}`;
 
         // Fetch the screenshot
         const webimage = await axios.get(screenshotApi, { responseType: 'arraybuffer' });
@@ -131,6 +131,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, reply }) => 
         reply(`An error occurred: ${e.response?.data?.error || e.message}`);
     }
 });
+
 // repo commands 
 
 cmd({
