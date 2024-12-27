5891bd25f908ea2bc58e5587adca4a6c1f0fd309
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-27 20:05:26 +0100

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -4,6 +4,23 @@ const { fetchJson } = require('../lib/functions');
 
 cmd({
     pattern: "gpt",
+    desc: "ai chat.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+return reply(`${data.data}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+cmd({
+    pattern: "gpt-4",
     desc: "ai chat from chat gpt",
     category: "ai",
     filename: __filename
