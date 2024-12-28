5e7b0cb2e8fcdf256e48c9913e394fc287bf6b38
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-28 20:17:46 +0100

    Update plugins

---
+++ plugins/sticker.js
@@ -0,0 +1,63 @@
+const { Sticker, StickerTypes } = require('wa-sticker-formatter');
+const { cmd } = require('../command');
+const { getRandom } = require('../lib/functions');
+
+const imgmsg = 'Reply to a photo for sticker!'; // Default message when no image or sticker is found.
+
+const descg = 'It converts your replied photo to a sticker.'; // Description of the command.
+
+cmd({
+    pattern: 'sticker',
+    react: '🤹‍♀️',
+    alias: ['s', 'stic'],
+    desc: descg,
+    category: 'convert',
+    use: '.sticker <Reply to image>',
+    filename: __filename
+}, async (conn, mek, m, { from, reply, isCmd, command, args, q, isGroup, pushname }) => {
+    try {
+        const isQuotedImage = m.quoted && (m.quoted.type === 'imageMessage' || (m.quoted.type === 'viewOnceMessage' && m.quoted.msg.type === 'imageMessage'));
+        const isQuotedSticker = m.quoted && m.quoted.type === 'stickerMessage';
+
+        if ((m.type === 'imageMessage') || isQuotedImage) {
+            const nameJpg = getRandom('.jpg');
+            const imageBuffer = isQuotedImage ? await m.quoted.download() : await m.download();
+            await require('fs').promises.writeFile(nameJpg, imageBuffer);
+
+            let sticker = new Sticker(nameJpg, {
+                pack: pushname, // The pack name
+                author: '', // The author name
+                type: q.includes('--crop') || q.includes('-c') ? StickerTypes.CROPPED : StickerTypes.FULL,
+                categories: ['🤩', '🎉'], // The sticker category
+                id: '12345', // The sticker id
+                quality: 75, // The quality of the output file
+                background: 'transparent', // The sticker background color (only for full stickers)
+            });
+
+            const buffer = await sticker.toBuffer();
+            return conn.sendMessage(from, { sticker: buffer }, { quoted: mek });
+        } else if (isQuotedSticker) {
+            const nameWebp = getRandom('.webp');
+            const stickerBuffer = await m.quoted.download();
+            await require('fs').promises.writeFile(nameWebp, stickerBuffer);
+
+            let sticker = new Sticker(nameWebp, {
+                pack: pushname, // The pack name
+                author: '', // The author name
+                type: q.includes('--crop') || q.includes('-c') ? StickerTypes.CROPPED : StickerTypes.FULL,
+                categories: ['🤩', '🎉'], // The sticker category
+                id: '12345', // The sticker id
+                quality: 75, // The quality of the output file
+                background: 'transparent', // The sticker background color (only for full stickers)
+            });
+
+            const buffer = await sticker.toBuffer();
+            return conn.sendMessage(from, { sticker: buffer }, { quoted: mek });
+        } else {
+            return await reply(imgmsg); // Return the default message if no image or sticker is found.
+        }
+    } catch (e) {
+        reply('Error !!');
+        console.error(e);
+    }
+});
