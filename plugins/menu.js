1252bd96b8e552596094246d40c3108d75f50a9e
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-29 15:20:30 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -13,7 +13,7 @@ cmd({
     category: "main",
     filename: __filename
 },
-async (conn, mek, m, { from, quoted, reply }) => {
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
     try {
         // Dynamic command categories
         let menu = {
