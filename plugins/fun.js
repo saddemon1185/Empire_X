e604e68edd9fa03757ed41636e331a0270a04b4f
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-12-30 13:32:38 +0100

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -1,5 +1,5 @@
 const { cmd, commands } = require('../command');
-const { sleep, fetchJson } = require('../lib/functions');
+const { fetchJson } = require('../lib/functions');
 const msg = require('../lib/msg');
 const axios = require('axios'); // Import axios
 
@@ -12,45 +12,6 @@ async function get(url) {
         throw new Error(`HTTP error! status: ${error.response ? error.response.status : error.message}`);
     }
 }
-
-// Hack prank command
-cmd({
-    pattern: "hack",
-    desc: "Hacking prank",
-    category: "fun",
-    filename: __filename
-}, async (conn, mek, m, { from, reply }) => {
-    try {
-        const messages = [
-            "HAKI Injecting Malware",
-            " █ 10%",
-            " █ █ 20%",
-            " █ █ █ 30%",
-            " █ █ █ █ 40%",
-            " █ █ █ █ █ 50%",
-            " █ █ █ █ █ █ 60%",
-            " █ █ █ █ █ █ █ 70%",
-            " █ █ █ █ █ █ █ █ 80%",
-            " █ █ █ █ █ █ █ █ █ 90%",
-            "System hijacking on process.. \\n Connecting to Server error to find 404 ",
-            "Device successfully connected... \\n Receiving data...",
-            "Data hijacked from device 100% completed \\n Killing all evidence, killing all malware...",
-            "HACKING COMPLETED",
-            "SENDING LOG DOCUMENTS...",
-            "SUCCESSFULLY SENT DATA AND Connection disconnected",
-            "BACKLOGS CLEARED"
-        ];
-
-        for (const message of messages) {
-            await conn.sendMessage(from, { text: message }, { quoted: mek });
-            await sleep(1000); // Delay between messages
-        }
-
-    } catch (e) {
-        console.log(e);
-        await reply(`❌ An error occurred: ${e}`);
-    }
-});
 
 // Rizz command
 cmd({
