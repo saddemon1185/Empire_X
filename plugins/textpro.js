61bd4502697b54dc1e32cf195f2670fa9445b0f0
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2025-01-04 21:28:07 +0100

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -299,31 +299,43 @@ cmd({
 });
 
 cmd({
-    pattern: "font",
-    alias: ["fancy"],
-    react: "✍️",
-    desc: "Convert text into various fonts.",
-    category: "textpro",
-    filename: __filename 
-}, async (_conn, _mek, _m, { from, quoted, body, args, q, reply }) => {
-    try {
-        
-        if (!q) return reply('Please provide text to convert into fonts.');
+   pattern: "fancy",
+   alias: ["font"],
+   react: "✍️",
+   desc: "Convert text into various fonts.",
+   category: "textpro",
+   filename: __filename
+}, async (conn, mek, m, { from, quoted, body, args, q, reply }) => {
+   try {
+      if (!q) return reply("Please provide text to convert into fonts. Eg .fancy Carmen");
 
-        
-        let response = await axios.get(`https://www.dark-yasiya-api.site/other/font?text=${encodeURIComponent(q)}`);
-        let result = response.data;
+      let response = await axios.get('https://www.dark-yasiya-api.site/other/font?text=' + encodeURIComponent(q));
+      let data = response.data;
 
-        if (!result.result) return reply('Error fetching fonts. Please try again later.');
+      if (!data.status) return reply("Error fetching fonts. Please try again later.");
 
-        let formattedFonts = result.result
-            .map(font => `*${font.name}:*\n${font.font}`) 
-            .join('\n\n'); 
+      let fontResults = data.result
+         .map(font => '*' + font.name + ":*\n" + font.result)
+         .join("\n\n");
 
-        reply(`*FANCY FONTS:*\n\n${formattedFonts}\n\n> *By Empire_X*`);
-    } catch (error) {
-        
-        console.error(error);
-        reply('An error occurred while fetching fonts.');
-    }
+      let message = `*EMPIRE_X FONTS*:\n\n${fontResults}\n\n> *BY Empire_X*`;
+
+      await conn.sendMessage(from, {
+         text: message,
+         contextInfo: {
+            mentionedJid: [m.sender],
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+               newsletterJid: '120363337275149306@newsletter',
+                    newsletterName: "Empire_X",
+                    serverMessageId: 143
+            }
+         }
+      }, { quoted: mek });
+
+   } catch (error) {
+      console.error(error);
+      reply("An error occurred while fetching fonts.");
+   }
 });
