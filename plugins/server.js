a702cee376abf924cc14b0d677b0c27f6e71c4ed
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2025-01-04 01:23:25 +0100

    Update server.js

--- plugins/server.js
+++ plugins/server.js
@@ -45,4 +45,7 @@ async (conn, mek, m, { text, isCreator, reply }) => {
         if (stdout) {
             return reply(`Output:\n${stdout}`);
         }
+    } catch (err) {
+        return reply(`Error: ${err.message}`);
+    }
 });
