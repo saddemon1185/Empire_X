76d3a60b360094a11bb17e10ef05bf3b064b2833
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2025-01-08 09:20:55 +0100

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -130,3 +130,23 @@ cmd({
     }
 });
 
+cmd({
+  pattern: "insult",
+  desc: "Get a random insult",
+  category: "fun",
+  react: "🤥",
+}, async (conn, m) => {
+  try {
+    let response = await axios.get('https://api.maher-zubair.tech/misc/insult');
+    let data = response.data;
+
+    if (!data || !data.insult) {
+      return await conn.sendMessage(m.key.remoteJid, { text: 'Unable to retrieve an insult. Please try again later.' });
+    }
+
+    let insult = data.insult;
+    await conn.sendMessage(m.key.remoteJid, { text: `*Insult:* ${insult}` });
+  } catch (error) {
+    await conn.sendMessage(m.key.remoteJid, { text: `Error: ${error.message || error}` });
+  }
+});
