6f9354d4a850ec25f93157ff64fa90f08920e9c5
saddemon1185 <148841848+saddemon1185@users.noreply.github.com>
2025-01-17 13:27:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "uQdHBSJZ#tVXqAn0KZ-dqwnSDRLUtx8EaXGOK-zrdsCBlEvS1uXs",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2348144250768",
-    AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || "false",
+    SESSION_ID: process.env.SESSION_ID || "bBtSGRBY#96BHMKyRqhg0x4gkDT5HlhOWKz25Xf0iegdXjF13q60",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254757959246",
+    AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || "true",
     MODE: process.env.MODE || "private",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/r4decc.jpg",
     PREFIX: process.env.PREFIX || ".",
@@ -17,9 +17,9 @@ module.exports = {
     OWNER_NAME: process.env.OWNER_NAME || "𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
     BOT_NAME: process.env.BOT_NAME || "Empire_X",
     CAPTION: process.env.CAPTION || "Made By 𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
-    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "false",
-    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
-    STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅ Status Viewed By Empire_X",
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
+    STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅ i Reincarnated uchiha viewed your status and damn sad demon was right you are areal Gem i loved what i saw",
     AUTO_LIKE_EMOJI: process.env.AUTO_LIKE_EMOJI || "💜",
     ANTILINK: process.env.ANTILINK || "false",
 };
